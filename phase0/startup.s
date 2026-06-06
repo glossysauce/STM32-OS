6499729e29e6f22815dbfe50faa23fd6d9695040
sophie yang mac <121143211+glossysauce@users.noreply.github.com>
2026-06-05 22:41:41 -0700

    finish startup

--- phase0/startup.s
+++ phase0/startup.s
@@ -12,13 +12,13 @@ vector_table:
         .word MemManage_Handler
         .word BusFault_Handler
         .word UsageFault_Handler
-        .word 0        @ reserved
-        .word 0        @ reserved
-        .word 0        @ reserved
-        .word 0        @ reserved
+        .word 0 @ reserved
+        .word 0 @ reserved
+        .word 0 @ reserved
+        .word 0 @ reserved
         .word SVC_Handler
         .word DebugMon_Handler
-        .word 0        @ reserved
+        .word 0 @ reserved
         .word PendSV_Handler
         .word SysTick_Handler
 
@@ -36,9 +36,9 @@ data:
         cmp r0, r1
         blt data
         mov r3, #0
-zero_bss:
         ldr r0, =_sbss
         ldr r1, =_ebss
+zero_bss:
         str r3, [r0]
         add r0, r0, 4
         cmp r0, r1
@@ -46,3 +46,35 @@ zero_bss:
         bl main
 hang:
         b hang
+
+.section .text.Default_Handler
+.type Default_Handler, %function
+Default_Handler:
+    b Default_Handler
+
+.weak NMI_Handler
+.thumb_set NMI_Handler, Default_Handler
+
+.weak HardFault_Handler
+.thumb_set HardFault_Handler, Default_Handler
+
+.weak MemManage_Handler
+.thumb_set MemManage_Handler, Default_Handler
+
+.weak BusFault_Handler
+.thumb_set BusFault_Handler, Default_Handler
+
+.weak UsageFault_Handler
+.thumb_set UsageFault_Handler, Default_Handler
+
+.weak SVC_Handler
+.thumb_set SVC_Handler, Default_Handler
+
+.weak DebugMon_Handler
+.thumb_set DebugMon_Handler, Default_Handler
+
+.weak PendSV_Handler
+.thumb_set PendSV_Handler, Default_Handler
+
+.weak SysTick_Handler
+.thumb_set SysTick_Handler, Default_Handler
